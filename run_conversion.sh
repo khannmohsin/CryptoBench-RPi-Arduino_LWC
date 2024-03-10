d6af1749b37814d0e3a66f965e028df9282e98e8
khannmohsin <khann.mohsin@icloud.com>
2024-03-10 14:45:39 +0100

    Added a few more scripts

--- run_conversion.sh
+++ run_conversion.sh
@@ -18,3 +18,14 @@ gcc -fPIC -shared -o LW_Stream_Cipher/LWAE/ASCON/c_imp/main.so LW_Stream_Cipher/
 # -----------------C compilation of GRAIN CIPHER -----------------
 gcc -fPIC -shared -o LW_Stream_Cipher/LWAE/Grain128a/c_imp/grain128aead_32p.so LW_Stream_Cipher/LWAE/Grain128a/c_imp/grain128aead_32p.c
 
+# -----------------C compilation of MICKEY CIPHER -----------------
+gcc -fPIC -shared -o LW_Stream_Cipher/eSTREAM/HW_oriented/Mickey/c_imp/mickey2.so LW_Stream_Cipher/eSTREAM/HW_oriented/Mickey/c_imp/mickey2.c
+
+# -----------------C compilation of TRIVIUM CIPHER -----------------
+gcc -fPIC -shared -o LW_Stream_Cipher/eSTREAM/HW_oriented/Trivium/c_imp/trivium.so LW_Stream_Cipher/eSTREAM/HW_oriented/Trivium/c_imp/trivium.c
+
+# -----------------C compilation of SALSA CIPHER -----------------
+gcc -fPIC -shared -o LW_Stream_Cipher/eSTREAM/SW_oriented/Salsa/c_imp/ecrypt.so LW_Stream_Cipher/eSTREAM/SW_oriented/Salsa/c_imp/ecrypt.c
+
+# -----------------C compilation of SOSEMANUK CIPHER -----------------
+gcc -fPIC -shared -o LW_Stream_Cipher/eSTREAM/SW_oriented/Sosemanuk/c_imp/sosemanuk.so LW_Stream_Cipher/eSTREAM/SW_oriented/Sosemanuk/c_imp/sosemanuk.c
