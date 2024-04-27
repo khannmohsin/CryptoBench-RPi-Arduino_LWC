8d9e80c7e9e04ba7ad3f8a629674047872a5fafa
khannmohsin <khann.mohsin@icloud.com>
2024-04-27 20:48:52 +0200

    Added python scripts to shell script

--- rb_run_scripts.sh
+++ rb_run_scripts.sh
@@ -3,25 +3,53 @@ echo -e "\n"
 echo "-------------------------------------C IMPLEMENTATIONS -----------------------------------"
 
 # -----------------C compilation of AES CIPHER -----------------
-# python3 rb_main.py aes 128 Files/Crypto_input/video/video_2.mp4 128
+python3 rb_main.py aes 128 Files/Crypto_input/video/video_2.mp4 128
 
-# python3 rb_main.py aes 192 Files/Crypto_input/video/video_2.mp4 128
+python3 rb_main.py aes 192 Files/Crypto_input/video/video_2.mp4 128
 
-# python3 rb_main.py aes 256 Files/Crypto_input/video/video_2.mp4 128
+python3 rb_main.py aes 256 Files/Crypto_input/video/video_2.mp4 128
 
 # -----------------C Implementation of PRESENT CIPHER -----------------
-# python3 rb_main.py present 80 Files/Crypto_input/video/video_2.mp4 64
+python3 rb_main.py present 80 Files/Crypto_input/video/video_2.mp4 64
 
-# python3 rb_main.py present 128 Files/Crypto_input/video/video_2.mp4 64
+python3 rb_main.py present 128 Files/Crypto_input/video/video_2.mp4 64
 
+
+# -----------------C Implementation of XTEA CIPHER -----------------
+
+python3 rb_main.py xtea 128 Files/Crypto_input/video/video_2.mp4 64
+
+# -----------------C Implementation of SIMON CIPHER -----------------
+
+python3 rb_main.py simon 64 Files/Crypto_input/video/video_2.mp4 32
+
+python3 rb_main.py simon 96 Files/Crypto_input/video/video_2.mp4 48
+
+python3 rb_main.py simon 128 Files/Crypto_input/video/video_2.mp4 64
+
+python3 rb_main.py simon 144 Files/Crypto_input/video/video_2.mp4 96
+
+python3 rb_main.py simon 256 Files/Crypto_input/video/video_2.mp4 128
+
+# -----------------C Implementation of SPECK CIPHER -----------------
+
+python3 rb_main.py speck 64 Files/Crypto_input/video/video_2.mp4 32
+
+python3 rb_main.py speck 96 Files/Crypto_input/video/video_2.mp4 48
+
+python3 rb_main.py speck 128 Files/Crypto_input/video/video_2.mp4 64
+
+python3 rb_main.py speck 144 Files/Crypto_input/video/video_2.mp4 96
+
+python3 rb_main.py speck 256 Files/Crypto_input/video/video_2.mp4 128
 
 # -----------------C Implementation of CLEFIA CIPHER -----------------
 
-# python3 rb_main.py clefia 128 Files/Crypto_input/video/video_2.mp4 128
+python3 rb_main.py clefia 128 Files/Crypto_input/video/video_2.mp4 128
 
-# python3 rb_main.py clefia 192 Files/Crypto_input/video/video_2.mp4 128
+python3 rb_main.py clefia 192 Files/Crypto_input/video/video_2.mp4 128
 
-# python3 rb_main.py clefia 256 Files/Crypto_input/video/video_2.mp4 128
+python3 rb_main.py clefia 256 Files/Crypto_input/video/video_2.mp4 128
 
 
 # -----------------C Implementation of Ascon CIPHER -----------------
@@ -30,25 +58,25 @@ echo "-------------------------------------C IMPLEMENTATIONS -------------------
 
 # -----------------C Implementation of Grain128a CIPHER -----------------
 
-# python3 rb_main.py grain-128a 128 Files/Crypto_input/video/video_2.mp4 -
+python3 rb_main.py grain-128a 128 Files/Crypto_input/video/video_2.mp4 -
 
 # -----------------C Implementation of Grain-v1 CIPHER -----------------
 
 python3 rb_main.py grain-v1 80 Files/Crypto_input/video/video_2.mp4 -
 
-# -----------------C Implementation of Mickey-v2 CIPHER -----------------
+-----------------C Implementation of Mickey-v2 CIPHER -----------------
 
-# python3 rb_main.py mickey 80 Files/Crypto_input/video/video_2.mp4 -
+python3 rb_main.py mickey 80 Files/Crypto_input/video/video_2.mp4 -
 
-# -----------------C Implementation of Trivium CIPHER -----------------
+-----------------C Implementation of Trivium CIPHER -----------------
 
-# python3 rb_main.py trivium 80 Files/Crypto_input/video/video_2.mp4 -
+python3 rb_main.py trivium 80 Files/Crypto_input/video/video_2.mp4 -
 
-# -----------------C Implementation of Salsa20 CIPHER -----------------
+-----------------C Implementation of Salsa20 CIPHER -----------------
 
-# python3 rb_main.py salsa 128 Files/Crypto_input/video/video_2.mp4 -
+python3 rb_main.py salsa 128 Files/Crypto_input/video/video_2.mp4 -
 
-# -----------------C Implementation of Sosemanuk CIPHER -----------------
+-----------------C Implementation of Sosemanuk CIPHER -----------------
 
 python3 rb_main.py sosemanuk 128 Files/Crypto_input/video/video_2.mp4 -
 
--- rb_run_scripts_1mb.sh
+++ rb_run_scripts_1mb.sh
@@ -17,6 +17,33 @@ for ((i = 1; i <= iterations; i++)); do
 
     python3 rb_main.py present 128 Files/Crypto_input/photo/oneMB_photo.jpg 64
 
+    # -----------------C Implementation of X-TEA CIPHER -----------------
+
+    python3 rb_main.py xtea 128 Files/Crypto_input/photo/oneMB_photo.jpg 64
+
+    # -----------------C Implementation of SIMON CIPHER -----------------
+
+    python3 rb_main.py simon 64 Files/Crypto_input/photo/oneMB_photo.jpg 32
+
+    python3 rb_main.py simon 96 Files/Crypto_input/photo/oneMB_photo.jpg 48
+
+    python3 rb_main.py simon 128 Files/Crypto_input/photo/oneMB_photo.jpg 64
+
+    python3 rb_main.py simon 144 Files/Crypto_input/photo/oneMB_photo.jpg 96
+
+    python3 rb_main.py simon 256 Files/Crypto_input/photo/oneMB_photo.jpg 128
+
+    # -----------------C Implementation of SPECK CIPHER -----------------
+
+    python3 rb_main.py speck 64 Files/Crypto_input/photo/oneMB_photo.jpg 32
+
+    python3 rb_main.py speck 96 Files/Crypto_input/photo/oneMB_photo.jpg 48
+
+    python3 rb_main.py speck 128 Files/Crypto_input/photo/oneMB_photo.jpg 64
+
+    python3 rb_main.py speck 144 Files/Crypto_input/photo/oneMB_photo.jpg 96
+
+    python3 rb_main.py speck 256 Files/Crypto_input/photo/oneMB_photo.jpg 128
 
     # -----------------C Implementation of CLEFIA CIPHER -----------------
 
@@ -57,61 +84,61 @@ for ((i = 1; i <= iterations; i++)); do
 
 done
 
-echo -e "\n"
+# echo -e "\n"
 
-for ((i = 1; i <= iterations; i++)); do
-    echo "-------------------------------------PYTHON IMPLEMENTATIONS -----------------------------------"
+# for ((i = 1; i <= iterations; i++)); do
+#     echo "-------------------------------------PYTHON IMPLEMENTATIONS -----------------------------------"
 
-    # -----------------Python compilation of AES CIPHER -----------------
-    python3 rb_main.py py-aes 128 Files/Crypto_input/photo/oneMB_photo.jpg 128
+#     # -----------------Python compilation of AES CIPHER -----------------
+#     python3 rb_main.py py-aes 128 Files/Crypto_input/photo/oneMB_photo.jpg 128
 
-    python3 rb_main.py py-aes 192 Files/Crypto_input/photo/oneMB_photo.jpg 128
+#     python3 rb_main.py py-aes 192 Files/Crypto_input/photo/oneMB_photo.jpg 128
 
-    python3 rb_main.py py-aes 256 Files/Crypto_input/photo/oneMB_photo.jpg 128
+#     python3 rb_main.py py-aes 256 Files/Crypto_input/photo/oneMB_photo.jpg 128
 
-    # -----------------PYTHON Implementation of PRESENT CIPHER -----------------
-    python3 rb_main.py py-present 80 Files/Crypto_input/photo/oneMB_photo.jpg 64
+#     # -----------------PYTHON Implementation of PRESENT CIPHER -----------------
+#     python3 rb_main.py py-present 80 Files/Crypto_input/photo/oneMB_photo.jpg 64
 
-    python3 rb_main.py py-present 128 Files/Crypto_input/photo/oneMB_photo.jpg 64
+#     python3 rb_main.py py-present 128 Files/Crypto_input/photo/oneMB_photo.jpg 64
 
-    # -----------------Python Implementation of X-TEA CIPHER -----------------
+#     # -----------------Python Implementation of X-TEA CIPHER -----------------
 
-    python3 rb_main.py py-xtea 128 Files/Crypto_input/photo/oneMB_photo.jpg 64
+#     python3 rb_main.py py-xtea 128 Files/Crypto_input/photo/oneMB_photo.jpg 64
 
-    # -----------------Python Implementation of SIMON CIPHER -----------------
+#     # -----------------Python Implementation of SIMON CIPHER -----------------
 
-    python3 rb_main.py py-simon 64 Files/Crypto_input/photo/oneMB_photo.jpg 32
+#     python3 rb_main.py py-simon 64 Files/Crypto_input/photo/oneMB_photo.jpg 32
 
-    python3 rb_main.py py-simon 96 Files/Crypto_input/photo/oneMB_photo.jpg 48
+#     python3 rb_main.py py-simon 96 Files/Crypto_input/photo/oneMB_photo.jpg 48
 
-    python3 rb_main.py py-simon 96 Files/Crypto_input/photo/oneMB_photo.jpg 64
+#     python3 rb_main.py py-simon 96 Files/Crypto_input/photo/oneMB_photo.jpg 64
 
-    python3 rb_main.py py-simon 128 Files/Crypto_input/photo/oneMB_photo.jpg 64
+#     python3 rb_main.py py-simon 128 Files/Crypto_input/photo/oneMB_photo.jpg 64
 
-    python3 rb_main.py py-simon 96 Files/Crypto_input/photo/oneMB_photo.jpg 96
+#     python3 rb_main.py py-simon 96 Files/Crypto_input/photo/oneMB_photo.jpg 96
 
-    python3 rb_main.py py-simon 128 Files/Crypto_input/photo/oneMB_photo.jpg 128
+#     python3 rb_main.py py-simon 128 Files/Crypto_input/photo/oneMB_photo.jpg 128
 
-    python3 rb_main.py py-simon 192 Files/Crypto_input/photo/oneMB_photo.jpg 128
+#     python3 rb_main.py py-simon 192 Files/Crypto_input/photo/oneMB_photo.jpg 128
 
-    python3 rb_main.py py-simon 256 Files/Crypto_input/photo/oneMB_photo.jpg 128
+#     python3 rb_main.py py-simon 256 Files/Crypto_input/photo/oneMB_photo.jpg 128
 
-    # -----------------Python Implementation of SPECK CIPHER -----------------
+#     # -----------------Python Implementation of SPECK CIPHER -----------------
 
-    python3 rb_main.py py-speck 64   32
+#     python3 rb_main.py py-speck 64   32
 
-    python3 rb_main.py py-speck 96 Files/Crypto_input/photo/oneMB_photo.jpg 48
+#     python3 rb_main.py py-speck 96 Files/Crypto_input/photo/oneMB_photo.jpg 48
 
-    python3 rb_main.py py-speck 96 Files/Crypto_input/photo/oneMB_photo.jpg 64
+#     python3 rb_main.py py-speck 96 Files/Crypto_input/photo/oneMB_photo.jpg 64
 
-    python3 rb_main.py py-speck 128 Files/Crypto_input/photo/oneMB_photo.jpg 64
+#     python3 rb_main.py py-speck 128 Files/Crypto_input/photo/oneMB_photo.jpg 64
 
-    python3 rb_main.py py-speck 96 Files/Crypto_input/photo/oneMB_photo.jpg 96
+#     python3 rb_main.py py-speck 96 Files/Crypto_input/photo/oneMB_photo.jpg 96
 
-    python3 rb_main.py py-speck 128 Files/Crypto_input/photo/oneMB_photo.jpg 128
+#     python3 rb_main.py py-speck 128 Files/Crypto_input/photo/oneMB_photo.jpg 128
 
-    python3 rb_main.py py-speck 192 Files/Crypto_input/photo/oneMB_photo.jpg 128
+#     python3 rb_main.py py-speck 192 Files/Crypto_input/photo/oneMB_photo.jpg 128
 
-    python3 rb_main.py py-speck 256 Files/Crypto_input/photo/oneMB_photo.jpg 128
+#     python3 rb_main.py py-speck 256 Files/Crypto_input/photo/oneMB_photo.jpg 128
 
-done
+# done
